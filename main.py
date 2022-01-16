1e96f672c67bc4ceb3e792d7ffb96dd9d28f856a
Xuming <voicevon@gmail.com>
2022-01-16 18:01:15 +0800

    add comment

--- main.py
+++ main.py
@@ -33,7 +33,14 @@ class AgvScheduler():
         return path
 
     @classmethod
-    def Plan(cls, task: Task, bot:AgvBotAgent):
+    def Plan(cls, task: Task, bot:AgvBotAgent) -> bool:
+        '''
+        Core function of the system.
+        Mainly will do:
+            1. Select an AGV, This has been done before invoking me.
+            2. Calculate Path-A: from current position to source station which is in the task.
+            3. Calculate Path-B: from source staion to target station.
+        '''
         task.AgvBot = bot
         # calculate path to source point
         path_to_source = cls.GetTrackPath(bot.current_point, task.SourceStation_id)
@@ -41,9 +48,16 @@ class AgvScheduler():
         path_to_target = cls.GetTrackPath(bot.path_to_source,task.TargetStation_id)
         bot.StartTask(path_to_source, path_to_target)
 
+
     @classmethod
     def SpinOnce(cls) -> None:
         '''
+        Core function of the system.
+        Mainly will do:
+            1. Select an AGV
+            2. Calculate Path
+                2-1: the path from current position to source station which is in the task.
+                2-2: the path from source staion to target station.
         '''
         # deal task
         task = TaskQueue.FetchSingleTask()
@@ -76,7 +90,7 @@ if __name__ == '__main__':
         source_station = 2
         target_station = 3
         TaskQueue.AppendTask(task_id, source_station, target_station)
-        
+
         AgvScheduler.SpinOnce()
         print(TaskQueue.show())
         time.sleep(2)
